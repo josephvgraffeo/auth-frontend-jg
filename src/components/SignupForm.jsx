e40cf49d8db422a67ecb05745aa6c09da92a396e
Joseph Graffeo <josephvgraffeo1@gmail.com>
2023-02-09 10:26:00 -0500

    added context and signup form

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,3 +1,29 @@
+import { useState, useContext } from "react"
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../App"
+
 export default function SignupForm() {
-    return <h2>Signup Form</h2>
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const {setUser} = useContext(AuthContext);
+    const navigate = useNavigate();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setUser({email, password});
+        navigate('/secret')
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="email">Email &nbsp;
+                <input type="email" name="email" value={email} onChange={e => setEmail(e.target.value)}/>
+            </label>
+            <br />
+            <label htmlFor="password">Password &nbsp;
+                <input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)}/>
+            </label>
+            <input type="submit" value="Sign Up" />
+        </form>
+    )
 }
